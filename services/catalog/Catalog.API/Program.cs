c493ed9dadf82f7edf92dec927ed686f153fd1ef
Belal <BelalGit@gmail.com>
2026-07-02 05:41:06 +0300

    Add project files.

---
+++ services/catalog/Catalog.API/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "Catalog API for E-Commerce Microservice",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "belal Saied",
+            Email = "bellllyelnagggar225@gmail.com",
+            Url = new Uri("https://github.com/belsaied")
+        }
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
